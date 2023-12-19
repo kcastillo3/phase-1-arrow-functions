f82d55f598166851431eaaf5e25e6ba07334b12f
Kevin <kevincastillo333@gmail.com>
2023-12-19 01:35:53 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// function foo(){
+//     return 'bar'
+// }
+
+// const foo = function(){
+//     return 'bar'
+// }
+
+// const sum = 1 + 1
+// const difference = 10 - 1
+
+// const foo = function(){
+//     return 'bar'
+// }
+
+// const add = (parameter1, parameter2) => parameter1 + parameter2
+// add(2,3)
+
+// (parameter1, parameter2) => parameter1 + parameter2
+
+// const twoAdder = x => x + 2;
+// // is the same as
+// const twoAdder = (x) => x + 2;
+
+// const sum = (parameter1, parameter2) => {
+//     console.log(`Adding ${parameter1}`);
+//   console.log(`Adding ${parameter2}`);
+//   return parameter1 + parameter2;
+// }
+// sum(1,2); 
+
+// const nums = [1,2,3]
+// const square = nums.map(x => x ** 2)
+// squares;
+// nums;
+
+const divide = function(){
+    return 2000 / 100 
+}
+
+const square = x => x * x
+
+const add = (y,z) => y + z
